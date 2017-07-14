65f97fb25780a40b4cb83c5c08f5d481e073d477
Mark Tiedemann <www.marktiedemann@gmail.com>
2017-07-15 01:09:53 +0200

    Improve host up status discovery

--- scripts/i9n/start-host.js
+++ scripts/i9n/start-host.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const { spawn } = require('child_process')
 const fetch = require('fetch-retry')
-const address = 'http://localhost:7071/'
+const statusRoute = 'http://localhost:7071/admin/host/status'
 
 const host = spawn('func.cmd', ['host', 'start'], {
   detached: true,
@@ -9,11 +9,21 @@ const host = spawn('func.cmd', ['host', 'start'], {
   cwd: path.resolve('..', __dirname)
 })
 
-// wait max. 5 seconds until host is up
-fetch(address, { retries: 10, retryDelay: 500 }).then(() =>
-  // then wait another 500 ms to give the host
-  // some time to init the functions
-  setTimeout(() => {
-    host.unref()
-  }, 500)
-)
+checkHostUp()
+
+function checkHostUp() {
+  // See: https://github.com/Azure/azure-functions-cli/issues/185
+  const retryDelay = 300
+  fetch(statusRoute, { retries: 100, retryDelay })
+    .then(
+      res =>
+        res.status !== 200 ? Promise.reject(new Error(res.status)) : res.json()
+    )
+    .then(body => {
+      if (body.state !== 'Running') {
+        setTimeout(checkHostUp, retryDelay)
+      } else {
+        host.unref()
+      }
+    })
+}
