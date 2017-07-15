cdddab099da14b3abbeb0f82e944e9d7693b05aa
Mark Tiedemann <www.marktiedemann@gmail.com>
2017-07-15 17:43:13 +0200

    Minor refactorings

--- scripts/i9n/start-host.js
+++ scripts/i9n/start-host.js
@@ -1,18 +1,18 @@
 const path = require('path')
 const { spawn } = require('child_process')
 const fetch = require('fetch-retry')
-const statusRoute = 'http://localhost:7071/admin/host/status'
 
 const host = spawn('func.cmd', ['host', 'start'], {
   detached: true,
   stdio: 'ignore',
-  cwd: path.resolve('..', __dirname)
+  cwd: path.resolve('../..', __dirname)
 })
 
 checkHostUp()
 
 function checkHostUp() {
   // See: https://github.com/Azure/azure-functions-cli/issues/185
+  const statusRoute = 'http://localhost:7071/admin/host/status'
   const retryDelay = 300
   fetch(statusRoute, { retries: 100, retryDelay })
     .then(
